5d315520ec11c317ee046e75b13667033b6da438
Nilot Paul <nilotpalbhuyan352@tutanota.com>
2018-05-27 02:52:28 -0400

    added python progs from another repo to this one.

---
+++ Python /Divide.py
@@ -0,0 +1,6 @@
+print "enter two numbers"
+num1 = input()
+num2 = input()
+result = num1/num2
+print "the result is "
+print result
---
+++ Python /add.py
@@ -0,0 +1,6 @@
+print "enter 2 numbers"
+num1 = input()
+num2 = input()
+result = num1 + num2
+print "the result is" 
+print result
