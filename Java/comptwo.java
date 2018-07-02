70157bb2ada241eed644af540fd99ba02ccace29
Nilot Paul <nilotpalbhuyan352@tutanota.com>
2018-07-02 03:53:15 -0400

    java compare program
    Java program to compare two numbers.

---
+++ Java/comptwo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class comptwo
+{
+	public static void main(String args[])
+	{
+	int a=5;
+	int b=7;
+	if(a>b)
+		System.out.println("A is greater than B");
+		else
+			System.out.println("B is greater than A");
+	}
+}
+		
